fb1b5a0046fa27b92185e6dab79b7b3a2222bee2
claramartins2002 <claramartins.cm57@gmail.com>
2021-04-10 14:09:41 -0300

    primeiro commit

---
+++ bank/Conta.cs
@@ -0,0 +1,56 @@
+﻿using bank.Enum;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace bank
+{
+    class Conta
+    {
+        private TipoConta tipoConta { get; set; }
+        private string Nome { get; set; }
+        private double Credito { get; set; }
+        private double Saldo { get; set; }
+
+        public Conta(TipoConta tipoConta, double saldo, double credito, string nome)
+        {
+            this.tipoConta = tipoConta;
+            this.Saldo = saldo;
+            this.Credito = credito;
+            this.Nome = nome;
+        }
+        public bool Sacar(double valorSaque)
+        {
+            if (this.Saldo - valorSaque < (this.Credito * -1))
+            {
+                Console.WriteLine("Saldo insuficiente");
+                return false;
+            }
+            this.Saldo -= valorSaque;
+            Console.WriteLine("Saldo atual da conta de {0} é {1}", this.Nome, this.Saldo);
+            return true;
+        }
+
+        
+        public void Depositar(double valorDepositado) {
+            this.Saldo += valorDepositado;
+            Console.WriteLine("Saldo atual da conta de {0} é {1}", this.Nome, this.Saldo);
+        }
+        public void Transferir(double valorTransferencia, Conta contadestino)
+        {
+            if (this.Sacar(valorTransferencia))
+            {
+                contadestino.Depositar(valorTransferencia);
+            }
+        }
+        public override string ToString()
+        {
+            string retorno = "";
+            retorno += "TipoConta " + this.tipoConta + " | ";
+            retorno += "Nome " + this.Nome + " | ";
+            retorno += "Saldo " + this.Saldo + " | ";
+            retorno += "Crédito " + this.Credito + " | ";
+            return retorno;
+        }
+    }
+}
